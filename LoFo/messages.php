156edff114b13f3d150acfc151ce749ebcc052df
Dante Razvan <mst_marik@yahoo.com>
2017-06-10 02:45:46 +0300

    we've added the contact method, via PM

---
+++ LoFo/messages.php
@@ -0,0 +1,259 @@
+<?php
+include_once("php_includes/check_login_status.php");
+if($user_ok == false)
+    {
+        header("location: signup.php");
+        exit();
+    }
+ ?>
+ <?php
+
+// Apel Ajax
+if(isset($_POST["reported_username"])){
+    // Conecatere la BD
+	include_once("php_includes/db_con.php");
+    
+    //Inserare obiect in tabel
+$reported_username = $_POST['reported_username']; //$numeOb
+$reason = $_POST['reason']; //$categorie
+$described_report =$_POST['described_report']; //$producer
+$reported_by = $_SESSION["username"];
+$report_time = date("Y/m/d"); // date
+    
+    
+        
+    $sql = mysqli_prepare($db_con,"INSERT INTO reported_by (`reported_username`, `reason`, `described_report`, `reported_by`, `report_time`) VALUES(?, ?, ?, ?, ?)");
+	mysqli_stmt_bind_param($sql,'sssss', $reported_username, $reason, $described_report, $reported_by, $report_time);
+    
+    mysqli_stmt_execute($sql);
+
+    if(mysqli_stmt_affected_rows($sql) == 1){
+        
+		 echo 'Report trimis'; 
+         mysqli_stmt_close($sql);  
+         exit(); 
+	} else {
+        
+        echo 'Reportul n-a putut fi trimis';
+        mysqli_stmt_close($sql);
+        exit();
+    }
+
+    }
+   
+?>
+
+        <html>
+
+        <head>
+            <title>Lost and Found - Marius Râncu şi Nedelcu Răzvan</title>
+            <link rel="stylesheet" type="text/css" href="css/style.css">
+            <script src="js/main.js"></script>
+            <script src="js/ajax.js"></script>
+            <script>
+                function emptyElement(x) {
+                    _(x).innerHTML = "";
+                }
+
+                function adauga() {
+                    var reported_username = _("reported_username").value;
+                    var reason = _("reason").value;
+                    var described_report = _("described_report").value;
+//                    var reported_by = _("reported_by").value;
+//                    var reported_time = _("report_time").value;
+                    var status = _("status");
+
+                    if (reported_username == "" || reason == "" || described_report == "") {
+                        status.innerHTML = "Completati campurile obligatorii ! ";
+                    } else {
+
+                        var ajax = ajaxObj("POST", "report.php");
+                        ajax.onreadystatechange = function() {
+                            if (ajaxReturn(ajax) == true) {
+
+                                status.innerHTML = ajax.responseText;
+
+                            }
+                        }
+                        ajax.send("reported_username=" + reported_username + "&reason=" + reason + "&described_report=" + described_report);
+                    }
+                }
+            </script>
+        </head>
+
+        <body>
+            <div class="header_menu">
+                <div class="menu_content">
+                    <div class="menu_items">
+                        <a href="index.php">HOME</a> |
+                        <a href="signup.php">SIGN UP</a> |
+                        <a href="report.php" class="activ">REPORT</a> |
+                        <a href="my_profile.php">MY PROFILE</a> |
+                        <?php if($user_role == true) : ?>
+                        <a href="admin_panel.php">ADMIN PANEL</a> |
+                        <?php endif; ?>
+                        <a href="contact_us.php">CONTACT US</a>
+                    </div>
+
+                </div>
+            </div>
+            <div class="banner_bg">
+                <div class="banner_itself">
+                    <div class="slogan">
+                        <div class="big_text">L O F O</div>
+                        <div class="little_text">Lost & Found V4.0.1 BETA</div>
+                    </div>
+                    <?php if($user_ok == true) : ?>
+                    <div class="login">
+                        <div class="login_items">
+                            <span class="login_items">Welcome, <a href="./my_profile.php"><?php echo $log_username; ?></a>!
+                            <a href="logout.php" style="color: red; position: relative; float: right; right: 10px;top: 45px;">Logout</a></span>
+
+                        </div>
+                    </div>
+                    <?php else : ?>
+                    <div class="login">
+                        <div class="login_items">
+                            <input type="text" placeholder="Enter Username" id="username" size="18" required/>
+                            <input type="checkbox" checked="checked" id="remember"><span>Remember me?</span>
+                            <input type="password" placeholder="Enter Password" id="password" size="18" required/>
+                            <input type="submit" id="loginbtn" onclick="login()"></submit>
+                            <p id="status"></p>
+                        </div>
+                    </div>
+                    <?php endif; ?>
+                </div>
+            </div>
+            <div class="container">
+                <form class="form" onsubmit="return false;">
+                    <h1>INBOX</h1>
+                    <?php
+include_once("php_includes/check_login_status.php");
+include_once("php_includes/db_con.php");
+?>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+        <title>Personal Messages</title>
+    </head>
+    <body>
+        <div class="content">
+<?php
+//We check if the user is logged
+if(isset($_SESSION['username']))
+{
+//We list his messages in a table
+//Two queries are executes, one for the unread messages and another for read messages
+    
+    // Check connection
+
+if (mysqli_connect_errno())
+{
+    die("Failed to connect to MySQL: " . mysqli_connect_error());
+}
+    
+//$result1 = mysql_query($db_con,'select m1.id, m1.title, m1.timestamp, count(m2.id) as reps, users.user_id as userid, users.username from pm as m1, pm as m2,users where ((m1.user1="'.$log_user_id.'" and m1.user1read="no" and users.user_id=m1.user2) or (m1.user2="'.$log_user_id.'" and m1.user2read="no" and users.user_id=m1.user1)) and m1.id2="1" and m2.id=m1.id group by m1.id order by m1.id desc');
+    
+if (!$result1 = mysqli_query($db_con,'select m1.id, m1.title, m1.timestamp, count(m2.id) as reps, users.user_id as userid, users.username from pm as m1, pm as m2,users where ((m1.user1="'.$log_user_id.'" and m1.user1read="no" and users.user_id=m1.user2) or (m1.user2="'.$log_user_id.'" and m1.user2read="no" and users.user_id=m1.user1)) and m1.id2="1" and m2.id=m1.id group by m1.id order by m1.id desc'))
+{
+    die("Error" . mysqli_error($db_con));
+}
+    
+    $rowcount1=mysqli_num_rows($result1);
+    
+$result1_count = mysqli_num_rows($result1);
+    
+//$req2 = mysql_query($db_con, 'select m1.id, m1.title, m1.timestamp, count(m2.id) as reps, users.user_id as userid, users.username from pm as m1, pm as m2,users where ((m1.user1="2" and m1.user1read="yes" and users.user_id=m1.user2) or (m1.user2="2" and m1.user2read="yes" and users.user_id=m1.user1)) and m1.id2="1" and m2.id=m1.id group by m1.id order by m1.id desc');
+    if (!$result2 = mysqli_query($db_con,'select m1.id, m1.title, m1.timestamp, count(m2.id) as reps, users.user_id as userid, users.username from pm as m1, pm as m2,users where ((m1.user1="2" and m1.user1read="yes" and users.user_id=m1.user2) or (m1.user2="2" and m1.user2read="yes" and users.user_id=m1.user1)) and m1.id2="1" and m2.id=m1.id group by m1.id order by m1.id desc'))
+{
+    die("Error" . mysqli_error($db_con));
+}
+    
+    $rowcount2=mysqli_num_rows($result2);
+    
+    
+?>
+This is the list of your messages:<br />
+<a href="new_message.php" class="link_new_pm">New PM</a><br />
+<h3>Unread Messages(<?php echo $rowcount1; ?>):</h3>
+<table>
+        <tr>
+        <th class="title_cell">Title</th>
+        <th>Nb. Replies</th>
+        <th>Participant</th>
+        <th>Date of creation</th>
+    </tr>
+<?php
+//We display the list of unread messages
+while($row = mysqli_fetch_array($result1))
+{
+?>
+        <tr>
+        <td class="left"><a href="discussion.php?id=<?php echo $row['id']; ?>"><?php echo htmlentities($row['title'], ENT_QUOTES, 'UTF-8'); ?></a></td>
+        <td><?php echo $row['reps']-1; ?></td>
+        <td><a href="profile.php?id=<?php echo $row['userid']; ?>"><?php echo htmlentities($row['username'], ENT_QUOTES, 'UTF-8'); ?></a></td>
+        <td><?php echo date('Y/m/d H:i:s' ,$row['timestamp']); ?></td>
+    </tr>
+<?php
+}
+//If there is no unread message we notice it
+    echo $row;
+if( $row == 0 )
+{
+?>
+        <tr>
+        <td colspan="4" class="center">You have no unread message.</td>
+    </tr>
+<?php
+}
+?>
+</table>
+<br />
+<h3>Read Messages(<?php echo $rowcount2; ?>):</h3>
+<table border="1">
+        <tr>
+        <th class="title_cell">Title</th>
+        <th>Nb. Replies</th>
+        <th>Participant</th>
+        <th>Date or creation</th>
+    </tr>
+<?php
+//We display the list of read messages
+while($row = mysqli_fetch_array($result2))
+{
+?>
+        <tr>
+        <td class="left"><a href="discussion.php?id=<?php echo $row['id']; ?>"><?php echo htmlentities($row['title'], ENT_QUOTES, 'UTF-8'); ?></a></td>
+        <td><?php echo $row['reps']-1; ?></td>
+        <td><a href="profile.php?id=<?php echo $row['userid']; ?>"><?php echo htmlentities($row['username'], ENT_QUOTES, 'UTF-8'); ?></a></td>
+        <td><?php echo date('Y/m/d H:i:s' ,$row['timestamp']); ?></td>
+    </tr>
+<?php
+}
+//If there is no read message we notice it
+if($rowcount2==0)
+{
+?>
+        <tr>
+        <td colspan="4" class="center">You have no read message.</td>
+    </tr>
+<?php
+}
+?>
+</table>
+<?php
+}
+else
+{
+        echo 'You must be logged to access this page.';
+}
+?>
+                </div>
+        </body>
+</html>
+                </form>
+            </div>
+        </body>
+
+        </html>
---
+++ LoFo/new_message.php
@@ -0,0 +1,171 @@
+<?php
+include_once("php_includes/check_login_status.php");
+if($user_ok == false)
+    {
+        header("location: signup.php");
+        exit();
+    }
+ ?>
+    <?php
+//We check if the user is logged
+if(isset($_SESSION['username']))
+{    
+$form = true;
+$otitle = '';
+$orecip = '';
+$omessage = '';
+//We check if the form has been sent
+if(isset($_POST['title'], $_POST['recip'], $_POST['message']))
+{
+        $otitle = $_POST['title'];
+        $orecip = $_POST['recip'];
+        $omessage = $_POST['message'];
+        //We remove slashes depending on the configuration
+        if(get_magic_quotes_gpc())
+        {
+                $otitle = stripslashes($otitle);
+                $orecip = stripslashes($orecip);
+                $omessage = stripslashes($omessage);
+        }
+        //We check if all the fields are filled
+    
+        if($_POST['title']!='' and $_POST['recip']!='' and $_POST['message']!='')
+        {
+                //We protect the variables
+                $title = mysql_real_escape_string($otitle);
+                $recip = mysql_real_escape_string($orecip);
+                $message = mysql_real_escape_string(nl2br(htmlentities($omessage, ENT_QUOTES, 'UTF-8')));
+                //We check if the recipient exists
+                if (!$result = mysqli_query($db_con,'select count(user_id) as recip, user_id as recipid, (select count(*) from pm) as npm from users where username="'.$recip.'"'))
+{
+    die("Error: " . mysqli_error($db_con));
+} else { echo "merge";}
+            $rowcount=mysqli_num_rows($result);
+            while($row = mysqli_fetch_array($result))
+{
+    echo $recip_q = $row['recip'];
+    echo $recipid_q = $row['recipid'];
+    echo $npm_q = $row['npm'];
+}
+                if($recip_q==1)
+                {
+                        //We check if the recipient is not the actual user
+                        if($recipid_q != $log_user_id)
+                        {
+                                $id = $npm_q+1;
+                                //We send the message
+                            $insert = mysql_query('INSERT INTO pm (id, id2, title, user1, user2, message, timestamp, user1read, user2read) VALUES ("'.$id.'", "1", "'.$title.'", "'.$log_user_id.'", "'.$recipid_q.'", "'.$message.'", "'.time().'", "yes", "no")');
+                                if($insert)
+                                {
+?>
+        <div class="message">The message has successfully been sent.<br />
+            <a href="list_pm.php">List of my Personal messages</a></div>
+        <?php
+                                        $form = false;
+                                }
+                                else
+                                {
+                                        $error = 'An error occurred while sending the message';
+                                }
+                        }
+                        else
+                        {
+                                $error = 'You cannot send a message to yourself.';
+                        }
+                }
+                else
+                {
+                        $error = 'The recipient does not exists.';
+                }
+        }
+        else
+        {
+                $error = 'A field is empty. Please fill of the fields.';
+        }
+}
+elseif(isset($_GET['recip']))
+{
+
+        $orecip = $_GET['recip'];
+}
+if($form)
+{
+
+?>
+
+        <html>
+
+        <head>
+            <title>Lost and Found - Marius Râncu şi Nedelcu Răzvan</title>
+            <link rel="stylesheet" type="text/css" href="./css/style.css">
+        </head>
+
+        <body>
+            <div class="header_menu">
+                <div class="menu_content">
+                    <div class="menu_items">
+                        <a href="index.php">HOME</a> |
+                        <a href="signup.php">SIGN UP</a> |
+                        <a href="report.php" class="activ">REPORT</a> |
+                        <a href="my_profile.php">MY PROFILE</a> |
+                        <?php if($user_role == true) : ?>
+                        <a href="admin_panel.php">ADMIN PANEL</a> |
+                        <?php endif; ?>
+                        <a href="contact_us.php">CONTACT US</a>
+                    </div>
+
+                </div>
+            </div>
+            <div class="banner_bg">
+                <div class="banner_itself">
+                    <div class="slogan">
+                        <div class="big_text">L O F O</div>
+                        <div class="little_text">Lost & Found V4.0.1 BETA</div>
+                    </div>
+                    <?php if($user_ok == true) : ?>
+                    <div class="login">
+                        <div class="login_items">
+                            <span class="login_items">Welcome, <a href="./my_profile.php"><?php echo $log_username; ?></a>!
+                            <a href="logout.php" style="color: red; position: relative; float: right; right: 10px;top: 45px;">Logout</a></span>
+
+                        </div>
+                    </div>
+                    <?php else : ?>
+                    <div class="login">
+                        <div class="login_items">
+                            <input type="text" placeholder="Enter Username" id="username" size="18" required/>
+                            <input type="checkbox" checked="checked" id="remember"><span>Remember me?</span>
+                            <input type="password" placeholder="Enter Password" id="password" size="18" required/>
+                            <input type="submit" id="loginbtn" onclick="login()"></submit>
+                            <p id="status"></p>
+                        </div>
+                    </div>
+                    <?php endif; ?>
+                </div>
+            </div>
+            <div class="container">
+                <form class="form" action="new_message.php" method="post">
+                    <h1>Send a new PM</h1>
+                    <?php 
+ if(isset($error))
+{
+        echo '<div class="message_eroare">'.$error.'</div>';
+}
+ ?>
+                    <span>Titlu </span><input type="text" value="<?php echo htmlentities($otitle, ENT_QUOTES, 'UTF-8'); ?>" id="title" name="title" size="55"  /><br><br>
+                    <span>Username: </span><input type="text" value="<?php echo htmlentities($orecip, ENT_QUOTES, 'UTF-8'); ?>" id="recip" name="recip"  size="55"  /><br><br>
+                   <span>Message: </span> <textarea cols="57" rows="5" id="message" name="message"><?php echo htmlentities($omessage, ENT_QUOTES, 'UTF-8'); ?></textarea>
+                    <input type="submit" value="Send" />
+                </form>
+            </div>
+        <<?php
+}
+}
+else
+{
+        echo '<div class="message-eroare">You must be logged to access this page.</div>';
+}
+?>
+</body>
+
+</html>
