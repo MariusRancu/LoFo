57b24c3450c655458cc74be6eb12fdcb49400152
Dante Razvan <mst_marik@yahoo.com>
2017-06-17 03:01:45 +0300

    solved conflict between read/unread no

--- LoFo/php_includes/chat_notif.php
+++ LoFo/php_includes/chat_notif.php
@@ -7,14 +7,6 @@ function unread_messages(){
     echo $rowcount1;
 }
 
-echo 
-
-function read_messages(){
-    include("db_con.php");
-    include("check_login_status.php");
-    $result2 = mysqli_query($db_con,'select m1.id, m1.title, m1.timestamp, count(m2.id) as reps, users.user_id as userid, users.username from pm as m1, pm as m2,users where ((m1.user1="'.$log_user_id.'" and m1.user1read="yes" and users.user_id=m1.user2) or (m1.user2="'.$log_user_id.'" and m1.user2read="yes" and users.user_id=m1.user1)) and m1.id2="1" and m2.id=m1.id group by m1.id order by m1.id desc');
-    $rowcount2=mysqli_num_rows($result2);
-    echo $rowcount2;
-}
+unread_messages();
 
 ?>
